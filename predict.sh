6fdc7a7f108d6be696c67542705a93e6f8d19334
FridrichMethod <zhaoyangli@stanford.edu>
2026-01-13 13:36:24 -0800

    Remove batch_predict.sh script and update predict.sh to support batch processing directly from a directory of input files.

--- predict.sh
+++ predict.sh
@@ -2,6 +2,13 @@
 
 # This script is used to predict the affinity of a protein-ligand complex using Boltz.
 
+# $1: input file/directory
+# $2: output directory
+
+# If $1 is a directory, run batch prediction on all .yaml files in the directory.
+# boltz will create a subdirectory in $2 with the name of boltz_results_<input_file_name>.
+# so no need to specify the output directory for each input file.
+
 boltz predict "$1" \
     --out_dir "$2" \
     --accelerator gpu \
@@ -15,10 +22,11 @@ boltz predict "$1" \
     --num_subsampled_msa 1024 \
     --num_workers 4 \
     --output_format pdb \
+    --preprocessing-threads "$(nproc)" \
     --recycling_steps 3 \
     --subsample_msa \
     --use_potentials \
     --write_full_pae \
     --write_full_pde
-    # --override \
-    # --use_msa_server \
+# --override \
+# --use_msa_server \
