5513b7109522ad9da43ea40f7d810cddb3669807
FridrichMethod <zhaoyangli@stanford.edu>
2026-02-27 23:24:50 -0800

    Update  to use more processors

--- predict.sh
+++ predict.sh
@@ -10,21 +10,21 @@
 # so no need to specify the output directory for each input file.
 
 boltz predict "$1" \
-    --out_dir "$2" \
-    --accelerator gpu \
-    --affinity_mw_correction \
-    --cache ".cache/boltz" \
-    --devices 1 \
-    --diffusion_samples 10 \
-    --diffusion_samples_affinity 10 \
-    --max_parallel_samples 10 \
-    --num_workers 4 \
-    --output_format pdb \
-    --preprocessing-threads "$(nproc)" \
-    --recycling_steps 3 \
-    --use_potentials \
-    --write_full_pae \
-    --write_full_pde
+  --out_dir "$2" \
+  --accelerator gpu \
+  --affinity_mw_correction \
+  --cache ".cache/boltz" \
+  --devices 1 \
+  --diffusion_samples 25 \
+  --diffusion_samples_affinity 25 \
+  --max_parallel_samples 25 \
+  --num_workers "$(nproc)" \
+  --output_format pdb \
+  --preprocessing-threads "$(nproc)" \
+  --recycling_steps 10 \
+  --use_potentials \
+  --write_full_pae \
+  --write_full_pde
 # --max_msa_seqs 4096 \
 # --num_subsampled_msa 1024 \
 # --override \
